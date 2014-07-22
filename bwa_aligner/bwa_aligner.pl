cb792fc79309057596288027ff79fa7cf654c8f7
Darin London <darin.london@duke.edu>
2014-07-22 12:00:19 -0400

    modified usage and commandline arguments made the script a little more parallelizable, but still needs  the sai files to be unique across runs

--- bwa_aligner/bwa_aligner.pl
+++ bwa_aligner/bwa_aligner.pl
@@ -1,15 +1,58 @@
 #!/usr/bin/perl
 use strict;
+use Getopt::Std;
+use File::Basename;
 
-my $usage = $0." sanger_raw_seq build reference [-p sanger_pair_seq]\naligns both sanger_raw_seq and sanger_pair_seq as paired end if -p is provided\nThis container requires a data volume mapped to the container /data directory, using the --volumes-from or -v docker run flags. This directory must contain the sanger_raw_seq and sanger_pair_seq files.  It will produce a bam file in /data named for the sanger_raw_seq with .bam appended.  It will also produce intermediate sai and unsorted bam files in this directory, but will remove the unsorted bam file before exiting if everything runs smoothly.  Make sure /data maps to adquate storage.\nThe container also requires a reference volume mapped to the container /bwa_indexed directory, either using --volumes-from or -v.  Within this directory, you should organize your bwa_indexed reference fasta files into subdirectories named for the 'build' argument provided.  The aligner expects to align its input to /bwa_indexed/build/reference\n";
-my $raw_seq = shift or die $usage;
-my $build = shift or die $usage;
-my $reference = shift or die $usage;
-my $type = shift;
-my $pair_seq;
-if ($type) {
-  $pair_seq = shift or die $usage;
-}
+my $usage = q|docker run {data_volume} {reference_volume} dmlond/bwa_aligner {options}
+
+Aligns a fastq file, or set of paired end fastq files, to a reference genome using bwa
+and samtools to produce a sorted bam file.
+
+data_volume:  The container requires that the directory /data exist, and that all
+  file_names passed in with -s and -p be found in this directory.  This can be mounted
+  either with the -v or --volumes-from docker run directives.  If mounting a data
+  volume container, the container must specify /data as a VOLUME.  If mounting with
+  -v, use -v /path/to/hostdir:/data.
+  ** The aligner produces an sai file, final bam file, and many temporary bam files
+  that are created by samtools when it sorts the final bam file in this directory, 
+  so make sure there is enough storage space to handle this based on the size of the
+  input files.
+
+reference_volume: The container requires that the directory /bwa_indexed exist, with
+ a subdirectory named for the build specified with -b.  The reference file_name
+ specified with -R must be found in /bwa_indexed/${build}/ along with all of the
+ bwa index and samtools faidx index files.  This can be mounted either with
+ the -v or --volumes-from docker run directives.  If mounting a data volume
+ container, the container must specify /bwa_indexed as a VOLUME.  If mounting with
+ -v, use -v /path/to/hostdir:/bwa_indexed.
+
+options:
+  required:
+    -s file_name: name of sanger formatted fastq file
+      in /data/ (can be gzip compressed or uncompressed)
+    -b build: the name of the directory in /bwa_indexed/ where the reference
+      file specified with -R is located
+    -R file_name: name of the reference fastq file located in
+      /bwa_indexed/${build}/. This file must have been indexed with
+      both bwa, and samtools faidx, so that all of the accompanying
+      index files are located with this file in the same directory.
+      (can be gzip compressed or uncompressed)
+  optional:
+    -p file_name: if reads are paired_end, the second sanger formatted fastq file in the
+        pair located in /data/ (can be gzip compressed or uncompressed)
+    -o file_name: if specified the bam file produced will be named with this name in
+        /data/.  Defaults to the file_name passed in with the -s flag, with .bam appended.
+|;
+
+my $opt = {};
+Getopt::Std::getopts('s:b:R:p:o:', $opt);
+
+my $raw_seq = $opt->{s} or die $usage;
+my $build = $opt->{b} or die $usage;
+my $reference = $opt->{R} or die $usage;
+my $pair_seq = $opt->{p};
+my $final_bam_file = $opt->{o};
+
 my $raw_seq_path = "/data/${raw_seq}";
 unless (-f $raw_seq_path) {
   print STDERR "Cannot find ${raw_seq_path}\nPerhaps you need to include a DATA volume?\n";
@@ -41,16 +84,23 @@ unless (-f "${reference_path}.fai") {
   exit(1);
 }
 
-if ($type) {
+if ($final_bam_file) {
+  $final_bam_file = join('/', '/data', $final_bam_file);
+} else {
+  $final_bam_file = "${raw_seq_path}.bam";
+}
+
+if ($pair_seq) {
   paired();
 } else {
   single();
 }
+sort_bam();
 exit;
 
 sub single {
   my $output_sai = "${raw_seq_path}.sai";
-  my $output_bam = "${raw_seq_path}.bam";
+
   print STDERR "ALIGNING\n";
   `bwa aln ${reference_path} ${raw_seq_path} > ${output_sai}`;
   if ($?) {
@@ -58,18 +108,16 @@ sub single {
     exit(1);
   }
   print STDERR "GENERATING UNSORTED BAM\n";
-  `bwa samse ${reference_path} ${output_sai} ${raw_seq_path} | samtools view -bt ${reference_path}.fai - > ${output_bam}.unsorted`;
+  `bwa samse ${reference_path} ${output_sai} ${raw_seq_path} | samtools view -bt ${reference_path}.fai - > ${final_bam_file}.unsorted`;
   if ($?) {
    print STDERR "Could not generate bam file $!\n";
    exit(1);
   }
-  sort_bam($output_bam);
 }
 
 sub paired {
   my $output_pair1_sai = "${raw_seq_path}.sai";
   my $output_pair2_sai = "${pair_seq_path}.sai";
-  my $output_bam = "${raw_seq_path}.bam";
 
   print STDERR "ALIGNING PAIR1\n";
   `bwa aln ${reference_path} ${raw_seq_path} > ${output_pair1_sai}`;
@@ -86,25 +134,22 @@ sub paired {
   }
 
   print STDERR "GENERATING UNSORTED BAM\n";
-  `bwa sampe ${reference_path} ${output_pair1_sai} ${output_pair2_sai} ${raw_seq_path} ${pair_seq_path} | samtools view -bt ${reference_path}.fai - > ${output_bam}.unsorted`;
+  `bwa sampe ${reference_path} ${output_pair1_sai} ${output_pair2_sai} ${raw_seq_path} ${pair_seq_path} | samtools view -bt ${reference_path}.fai - > ${final_bam_file}.unsorted`;
   if ($?) {
    print STDERR "Could not generate unsorted bam file $!\n";
    exit(1);
   }
-  sort_bam($output_bam);
 }
 
 sub sort_bam {
-  my $output_bam = shift;
   print STDERR "SORTING\n";
-  `samtools sort ${output_bam}.unsorted ${output_bam}`;
+  `samtools sort ${final_bam_file}.unsorted ${final_bam_file}`;
   if ($?) {
     print STDERR "Could not generate sorted bam file\n";
     exit(1);
  }
- #STUPID SAMTOOLS!!!!
- `mv ${output_bam}.bam ${output_bam}`;
- `rm ${output_bam}.unsorted`;
- system('samtools', 'flagstat', ${output_bam});
+ #STUPID SAMTOOLS sort appends .bam!!!!
+ `mv ${final_bam_file}.bam ${final_bam_file}`;
+ `rm ${final_bam_file}.unsorted`;
+ system('samtools', 'flagstat', ${final_bam_file});
 }
-
